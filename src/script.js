4c8cebc44f1091587b165961530f3294127caabc
Mark Aronin <steroidcat@gmail.com>
2021-09-01 00:07:44 -0400

    Added properties for tree generation
    - Using tube for this instead of cylinders. With tubes I can define a start and end point
    - Made a recursive method for generating the tree
    - Added GUI controls for controlling the properties of the tree generation

--- src/script.js
+++ src/script.js
@@ -2,12 +2,15 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import { Vector3 } from 'three'
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI()
+gui.width = 350
+
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -42,7 +45,7 @@ scene.add(directionalLight)
  * Materials
  */
 const material = new THREE.MeshStandardMaterial()
-
+const treeMaterial = new THREE.MeshNormalMaterial()
 
 /**
  * Plane
@@ -71,6 +74,83 @@ const sizes = {
 }
 
 
+/**
+ * Tree properties
+ */
+const treeProperties = {
+    stemToStemRatio: 0.8,
+    stemToBranchRatio: 0.5,
+    depth: 6,
+    angle: 60,
+    scatterAngle: 0,
+    branches: 6,
+    branchLength: 30
+}
+
+
+/** 
+ * Tree Group
+ */
+const tree = new THREE.Group()
+
+/**
+ * Clear the tree and dispose of the children
+ */
+const clearTree = () => {
+    tree.children.forEach(child => {
+        child.geometry.dispose()
+    });
+    tree.remove(...tree.children);
+}
+
+
+
+const generateTree = () => {
+    console.log("generate tree");
+
+    clearTree()
+    const makeBranch = (currentDepth, branchLength, rootPos) => {
+        if (currentDepth >= treeProperties.depth) return
+
+        const branchEndPos = new THREE.Vector3(0, rootPos.y + branchLength, 0)
+        const stemPath = new THREE.LineCurve3(rootPos, branchEndPos);
+
+        const stemColor = new THREE.Color(0, 1 - currentDepth / treeProperties.depth, 0)
+        const stem = new THREE.Mesh(
+            new THREE.TubeBufferGeometry(stemPath, 20, 0.5, 8, false),
+            new THREE.MeshBasicMaterial({ color: stemColor })
+        )
+
+        branchLength = branchLength * treeProperties.stemToBranchRatio
+
+        makeBranch(++currentDepth, branchLength, branchEndPos)
+        tree.add(stem)
+    }
+
+    makeBranch(0, treeProperties.branchLength, new THREE.Vector3(0, 0, 0))
+
+}
+
+scene.add(tree)
+generateTree()
+
+
+/**
+ * Tree Properties UI
+ */
+const guiTreePropertiesFolder = gui.addFolder("Tree Properties")
+guiTreePropertiesFolder.open()
+guiTreePropertiesFolder.add(treeProperties, "branchLength").min(0).max(50).step(.1).onChange(generateTree)
+guiTreePropertiesFolder.add(treeProperties, "stemToStemRatio").min(0.1).max(0.8).step(0.1).onChange(generateTree)
+guiTreePropertiesFolder.add(treeProperties, "stemToBranchRatio").min(0.4).max(0.9).step(0.1).onChange(generateTree)
+
+guiTreePropertiesFolder.add(treeProperties, "angle").min(30).max(60).step(5).onChange(generateTree)
+guiTreePropertiesFolder.add(treeProperties, "scatterAngle").min(0).max(360).step(5).onChange(generateTree)
+guiTreePropertiesFolder.add(treeProperties, "depth").min(0).max(5).step(1).onChange(generateTree)
+guiTreePropertiesFolder.add(treeProperties, "branches").min(3).max(10).step(1).onChange(generateTree)
+
+
+
 window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
@@ -91,16 +171,16 @@ window.addEventListener('resize', () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 10000)
-camera.position.x = 25
-camera.position.y = 25
-camera.position.z = 25
+camera.position.x = 0
+camera.position.y = 20
+camera.position.z = 85
 scene.add(camera)
 
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
-controls.enablePan = false;
+// controls.enablePan = false;
 
 
 /**
